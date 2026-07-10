6ff1543963e82895fb7cb8d29fcdcb747cada6e5
alessioferrmani-lgtm <alessioferrmani@gmail.com>
2026-07-10 14:17:19 +0200

    Add Edge Function CORS helper

---
+++ supabase/functions/_shared/cors.ts
@@ -0,0 +1,8 @@
+Exit code: 0
+Wall time: 0.5 seconds
+Output:
+export const corsHeaders = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Headers": "authorization, x-client-info, apikey, content-type",
+};
+
