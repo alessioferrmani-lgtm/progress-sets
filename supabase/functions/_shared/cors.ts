aa8290421201da959137293e46228056fcaa573a
alessioferrmani-lgtm <alessioferrmani@gmail.com>
2026-07-10 14:19:07 +0200

    Fix Edge Function helper encoding

--- supabase/functions/_shared/cors.ts
+++ supabase/functions/_shared/cors.ts
@@ -1,8 +1,4 @@
-Exit code: 0
-Wall time: 0.5 seconds
-Output:
 export const corsHeaders = {
   "Access-Control-Allow-Origin": "*",
   "Access-Control-Allow-Headers": "authorization, x-client-info, apikey, content-type",
 };
-
